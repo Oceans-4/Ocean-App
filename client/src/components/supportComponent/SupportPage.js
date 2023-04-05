4557c5bea32c49edfb4c09a4776915dc9815e321
Amer Kulovic <amer.kulovic@yahoo.com>
2023-04-05 01:02:33 -0500

    added link to footer and styled support page

--- client/src/components/supportComponent/SupportPage.js
+++ client/src/components/supportComponent/SupportPage.js
@@ -1,33 +1,37 @@
 const SupportPage = () => {
   return (
-    <div className="flex flex-wrap justify-center">
-      <div className="w-1/2 border-blue-500 border">
-        <h1 className="text-center">Donate</h1>
+    <div className="flex flex-wrap justify-center py-8">
+      <div className="w-5/12 max-md:w-11/12 border-blue-500 border mx-7">
+        <h1 className="text-center text-4xl font-bold text-[#0B2447]">Donate</h1>
         <div className="flex flex-row">
           <h2></h2>
           <div>$25</div>
           <div>$50</div>
           <div>$75</div>
         </div>
-        <div className="flex justify-between">
-          <label>First Name</label>
+        <div className="flex flex-wrap justify-between m-3 max-lg:flex-col">
+          <label className="font-bold">First Name</label>
           <input className="border-blue-500 border" />
-          <label>Last Name</label>
+          <label className="font-bold">Last Name</label>
           <input className="border-blue-500 border" />
         </div>
-        <label>Email</label>
-        <input className="border-blue-500 border w-full" />
-        <label>Street Address</label>
-        <input className="border-blue-500 border w-full" />
+        <div className="m-3">
+          <label className="font-bold">Email</label>
+          <input className="border-blue-500 border w-full" />
+        </div>
+        <div className="m-3">
+          <label className="font-bold">Street Address</label>
+          <input className="border-blue-500 border w-full" />
+        </div>
         <div className="flex justify-center">
-          <button>Donate!</button>
+          <button className="bg-blue-500 text-white p-3 m-4 font-bold">Donate!</button>
         </div>
       </div>
-      <div className="w-1/2 border-blue-500 border">
-        <h1>Volunteer</h1>
+      <div className="w-5/12 max-md:w-11/12 border-blue-500 border">
+        <h1 className="text-center text-4xl font-bold text-[#0B2447]">Volunteer</h1>
         <p>Help out the cause! Learn in what ways you can go out and help the ocean with us!</p>
         <div className="flex justify-center">
-          <button>Volunteer!</button>
+          <button className="bg-blue-500 text-white p-3 m-4 font-bold">Volunteer!</button>
         </div>
       </div>
     </div>
