c0b64fae9dffbfbe15d993da281bc20dbefd21d3
Amer Kulovic <amer.kulovic@yahoo.com>
2023-04-06 11:39:38 -0500

    added animation to HomeInfo pictures

--- client/src/components/homeComponents/HomeInfo.js
+++ client/src/components/homeComponents/HomeInfo.js
@@ -1,5 +1,6 @@
 import water from "./../../images/deep-blue.jpg";
 import beach from "./../../images/beach.jpg";
+import "./HomeInfo.css";
 import TextInfo from "./TextInfo";
 
 const HomeInfo = () => {
@@ -8,10 +9,10 @@ const HomeInfo = () => {
       <section className="m-6">
         <div className="flex flex-wrap py-8 justify-center">
           <TextInfo title="We can make a difference, this won't be our first time." text="Our leadership team bring years of experience to bear on the greatest challenge of our time. We’re results driven, with a proven record of previous successes." image={water} buttonText="Learn More" link="/about" />
-          <img style={{ height: "380px", width: "400px" }} src={water} />
+          <img style={{ height: "380px", width: "400px" }} className="animation" src={water} />
         </div>
         <div className="flex flex-wrap py-8 justify-center">
-          <img style={{ height: "380px", width: "400px" }} src={beach} className="max-xl:order-3" />
+          <img style={{ height: "380px", width: "400px" }} src={beach} className="max-xl:order-3 animation2" />
           <TextInfo title="Helping the Ocean is a team effort. What can you do to help?" text="You can support Ocean Buddies and our cause in numerous ways, by donating, volunteering in our effort, or just purchasing items in our shop, you are helping change the ocean and our environment." image={water} buttonText="Make a Change" link="/support" />
         </div>
       </section>
