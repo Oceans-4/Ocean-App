c792b2cd0aad9693ca153fc8aa3bc42c80f956a4
Amer Kulovic <amer.kulovic@yahoo.com>
2023-04-06 20:18:33 -0500

    created the mapping function for Shop

--- client/src/components/shopComponents/Shop.js
+++ client/src/components/shopComponents/Shop.js
@@ -1,12 +1,77 @@
 import stingray from "./../../images/stingray.jpg";
 
+const DUMMY_DATA = [
+  {
+    id: 1,
+    title: "Save the Ocean T-Shirt",
+    description: "A nice silk t-shirt made with love.",
+    imageURL: stingray,
+    price: "$50",
+  },
+  {
+    id: 2,
+    title: "Save the Ocean Pants",
+    description: "A nice leather pants made with love.",
+    imageURL: stingray,
+    price: "$60",
+  },
+  {
+    id: 3,
+    title: "Save the Ocean Watch",
+    description: "A nice watch made with love.",
+    imageURL: stingray,
+    price: "$25",
+  },
+  {
+    id: 4,
+    title: "Save the Ocean Watch",
+    description: "A nice watch made with love.",
+    imageURL: stingray,
+    price: "$25",
+  },
+  {
+    id: 5,
+    title: "Save the Ocean Watch",
+    description: "A nice watch made with love.",
+    imageURL: stingray,
+    price: "$25",
+  },
+  {
+    id: 6,
+    title: "Save the Ocean Watch",
+    description: "A nice watch made with love.",
+    imageURL: stingray,
+    price: "$25",
+  },
+];
+
 const Shop = () => {
   return (
     <>
-      {/* <img style={{ height: "800px", width: "100%" }} src={stingray} /> */}
-      <div className="p-40 h-screen bg-blue-700 text-center">
+      <div className="pt-32 pb-5 bg-blue-700 text-center">
         <h1 className="italic text-white text-xl ">All proceeds will be donated to the supporting charities.</h1>
       </div>
+      <section className="mb-40 flex flex-wrap justify-around mx-10 m-5">
+        {DUMMY_DATA.map((item) => (
+          <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow mb-5">
+            <a href="#">
+              <img class="p-8 rounded-t-lg" src={item.imageURL} alt="product image" />
+            </a>
+            <div class="px-5 pb-5">
+              <a href="#">
+                <h5 class="text-xl font-semibold tracking-tight text-gray-900">{item.description}</h5>
+              </a>
+              <div class="flex items-center mt-2.5 mb-5"></div>
+              <div class="flex items-center justify-between">
+                <span class="text-3xl font-bold text-gray-900">{item.price}</span>
+                <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                  Add to cart
+                </a>
+              </div>
+            </div>
+          </div>
+        ))}
+      </section>
     </>
   );
 };
