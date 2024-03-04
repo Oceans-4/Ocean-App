b1c026d114be9181229d85c62044489929246429
Amer Kulovic <amer.kulovic@yahoo.com>
2024-03-03 18:34:17 -0600

    edited design change for TextInfo component

--- client/src/components/homeComponents/TextInfo.js
+++ client/src/components/homeComponents/TextInfo.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const TextInfo = (props) => {
   return (
-    <div className="w-3/5 p-5 py-4 relative max-xl:text-center">
+    <div className="w-3/5 p-5 py-4 relative max-xl:text-center max-sm:w-fit">
       <h1 className="font-bold text-5xl text-[#0B2447] mx-2 max-ex:order-1 max-sm:text-4xl">{props.title}</h1>
       <p className="m-8 font-light mx-5 max-ex:order-2 max-xl:mx-2">{props.text}</p>
       <Link to={props.link}>
